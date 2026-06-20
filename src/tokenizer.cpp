ff6bd30e99bdfa9a9faaff635c4c2fc98aa46464
Philipe Caetano <philipe2015amancio@hotmail.com>
2026-06-20 16:52:42 -0300

    feat(tokenizer)/ word catch from phrase that comes of commentaries

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -1,3 +1,23 @@
 #include "tokenizer.hpp"
+#include <sstream>
+#include <vector>
+#include <string>
+
 
 using namespace std;
+
+vector<string> word_catch(string phrase){
+    stringstream ss(phrase);
+    string word;
+    vector<string> words;
+
+    while(ss >> word){
+        erase_if(word, [](char c){
+            return ispunct(c);
+        });
+
+        words.push_back(word);
+    }
+
+    return words;
+}

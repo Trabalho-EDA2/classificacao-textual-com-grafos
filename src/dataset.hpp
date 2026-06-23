a9a78a47f1a98403aae9d3e3f7264f48389630f5
Hugo Freitas Silva <videogame041@gmail.com>
2026-06-22 23:38:55 -0300

    feat(classifier): expose training and evaluation APIs
    Co-authored-by: HauedyWS <hauedyws@gmail.com>
    
    Co-authored-by: m4rllon <marllonpc33@gmail.com>
    
    Co-authored-by: Phill-Chill <philipe2015amancio@hotmail.com>
    
    Co-authored-by: Pedro-Henrique3 <pedrohenriquefreirerodrigues@gmail.com>

--- src/dataset.hpp
+++ src/dataset.hpp
@@ -3,11 +3,26 @@
 
 #include <string>
 #include <map>
-#include "new_graph.hpp"
+#include "graph.hpp"
 
 using namespace std;
 
-void catch_coment_and_value(string pathCSV, graph &g, map<string, node*> &dicionario, int &curr_index);
-string classify_review(string frase, graph &g, map<string, node*> &dicionario);
+void catch_coment_and_value(
+    string pathCSV,
+    graph &g,
+    map<string, node*> &dicionario,
+    int &curr_index,
+    int max_reviews);
+string classify_review(
+    string frase,
+    graph &g,
+    map<string, node*> &dicionario,
+    bool verbose = true);
+void test_accuracy(
+    string pathCSV,
+    graph &g,
+    map<string, node *> &dicionario,
+    int linhas_treino,
+    int linhas_teste);
 
 #endif
